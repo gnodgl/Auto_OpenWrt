2a4046d0975bc517b11c5aedd973862252447df7
gnodgl <llgdong@qq.com>
2022-09-20 23:15:13 +0800

    修改测试

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,27 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+
+# 添加openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd package/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
+
+# 添加ssr-plus
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+
+# 添加smartdns
+svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns package/smartdns
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-smartdns package/luci-app-smartdns
+
+#添加argon-config 使用 最新argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+# 添加主题edge neobird
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-edge package/luci-theme-edge
+git clone https://github.com/thinktip/luci-theme-neobird.git  package/luci-theme-neobird
